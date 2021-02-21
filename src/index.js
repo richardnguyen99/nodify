5b1041d52870c4ae772ad93346bac57b449cf746
richardnguyen99 <richard.ng0616@gmail.com>
2021-02-20 16:15:38 -0800

    Added basic UI setup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
 
 const app = express();
 
+app.use(express.static("views/static"));
+app.use(expressLayouts);
+app.set("layout", "./layouts/layout");
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
-  res.render("index");
+  res.render("index", {
+    title: "Home page"
+  });
 });
 
 app.listen(3000, function() {
