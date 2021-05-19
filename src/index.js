96cb18a7b86d12b371d893e65736857a1769743a
Richard Nguyen <richard.ng0616@gmail.com>
2021-05-19 09:25:51 -0700

    Initialized app

---
+++ src/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const express = require("express");
+
+const app = express();
+
+app.get("/", function (req, res) {
+    res.send("Hello, World");
+});
+
+app.listen(4000, function () {
+    console.log(`App is listening ...`);
+});
