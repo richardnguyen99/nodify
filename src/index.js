a8c35f69b2358a6e1f3fbd33ec05a3faa942967f
Richard Nguyen <richard.ng0616@gmail.com>
2021-05-19 17:26:26 -0700

    Added static folders for CSS/JS/Assets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv");
 
@@ -8,6 +9,7 @@ const app = express();
 // App configuration
 app.set("PORT", process.env.PORT || 4000);
 app.set("view engine", "pug");
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (_req, res) => {
   res.render("home", {
