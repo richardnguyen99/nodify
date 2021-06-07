3da0c426ee270e61e9fe2791e6831572d93800eb
Richard Nguyen <richard.ng0616@gmail.com>
2021-06-07 16:56:09 -0700

    Added insertion sort & selection sort pages

--- src/libs/addon.cc
+++ src/libs/addon.cc
@@ -134,12 +134,89 @@ Napi::Value GetBubbleSortAnimation(const Napi::CallbackInfo &info)
   return jsAnimation;
 }
 
+/**
+ * @brief Get the Insertion Sort Animation object
+ *
+ * @param info
+ * @return Napi::Value
+ */
+Napi::Value GetInsertionSortAnimation(const Napi::CallbackInfo &info)
+{
+  Napi::Env env = info.Env();
+
+  if (info.Length() < 1)
+  {
+    Napi::TypeError::New(env, "require at least one argument").ThrowAsJavaScriptException();
+    return env.Null();
+  }
+
+  if (!info[0].IsArray())
+  {
+    Napi::TypeError::New(env, "require argument as an array").ThrowAsJavaScriptException();
+    return env.Null();
+  }
+
+  nodify::Animation animation;
+  std::vector<int> native_array;
+  Napi::Array jsArray = info[0].As<Napi::Array>();
+
+  for (int i = 0; i < jsArray.Length(); i++)
+  {
+    native_array.push_back(Napi::Value(jsArray[i]).ToNumber().Int32Value());
+  }
+
+  nodify::get_insertion_sort_animation(std::begin(native_array), std::end(native_array), animation);
+  Napi::Array jsAnimation = Napi::Array::New(info.Env(), animation.sorting.size());
+
+  for (auto current = std::begin(animation.sorting); current < std::end(animation.sorting); ++current)
+  {
+    Napi::Array jsStep = nodify::ToJSArray(info, std::begin(*current), std::end(*current));
+
+    jsAnimation[static_cast<int>(std::distance(std::begin(animation.sorting), current))] = jsStep;
+  }
+
+  return jsAnimation;
+}
+
+Napi::Value InsertionSort(const Napi::CallbackInfo &info)
+{
+  Napi::Env env = info.Env();
+
+  if (info.Length() < 1)
+  {
+    Napi::TypeError::New(env, "require at least one argument").ThrowAsJavaScriptException();
+    return env.Null();
+  }
+
+  if (!info[0].IsArray())
+  {
+    Napi::TypeError::New(env, "require argument as an array").ThrowAsJavaScriptException();
+    return env.Null();
+  }
+
+  std::vector<int> native_array;
+  Napi::Array jsInputArray = info[0].As<Napi::Array>();
+
+  for (int i = 0; i < jsInputArray.Length(); i++)
+  {
+    native_array.push_back(Napi::Value(jsInputArray[i]).ToNumber().Int32Value());
+  }
+
+  nodify::insertion_sort(std::begin(native_array), std::end(native_array));
+
+  Napi::Array jsReturnArr = nodify::ToJSArray(info, std::begin(native_array), std::end(native_array));
+
+  return jsReturnArr;
+}
+
 Napi::Object Init(Napi::Env env, Napi::Object exports)
 {
   exports.Set(Napi::String::New(env, "hello"), Napi::Function::New(env, Method));
   exports.Set(Napi::String::New(env, "randomIntegers"), Napi::Function::New(env, CreateRandomIntegers));
   exports.Set(Napi::String::New(env, "bubbleSort"), Napi::Function::New(env, BubbleSort));
+  exports.Set(Napi::String::New(env, "insertionSort"), Napi::Function::New(env, BubbleSort));
   exports.Set(Napi::String::New(env, "getBubbleSortAnimation"), Napi::Function::New(env, GetBubbleSortAnimation));
+  exports.Set(Napi::String::New(env, "getInsertionSortAnimation"), Napi::Function::New(env, GetInsertionSortAnimation));
   return exports;
 }
 
--- src/libs/animation.h
+++ src/libs/animation.h
@@ -28,13 +28,13 @@ struct Animation
  * 1: swapping
  * -1: terminating
  *
- * @tparam Iter
+ * @tparam iter
  * @param begin
  * @param end
  * @param animation
  */
-template <typename Iter>
-void get_bubble_sort_animation(Iter begin, Iter end, Animation &animation)
+template <typename iter>
+void get_bubble_sort_animation(iter begin, iter end, Animation &animation)
 {
   bool swapping = true;
 
@@ -42,7 +42,7 @@ void get_bubble_sort_animation(Iter begin, Iter end, Animation &animation)
   {
     swapping = false;
 
-    for (Iter current = begin; current != end; ++current)
+    for (iter current = begin; current != end; ++current)
     {
       int index = static_cast<int>(std::distance(begin, current));
 
@@ -56,6 +56,66 @@ void get_bubble_sort_animation(Iter begin, Iter end, Animation &animation)
     }
   }
   animation.sorting.push_back({-1, 0, 0});
+}
+
+template <typename iter>
+void get_insertion_sort_animation(iter begin, iter end, Animation &animation)
+{
+  if (begin == end)
+    return;
+
+  for (iter next = std::next(begin); next != end; ++next)
+  {
+    for (iter current = next; current > begin; --current)
+    {
+      iter prev = std::prev(current);
+      int prev_index = static_cast<int>(std::distance(begin, prev));
+      int current_index = static_cast<int>(std::distance(begin, current));
+
+      animation.sorting.push_back({0, current_index, prev_index});
+      if (*current < *prev)
+      {
+        animation.sorting.push_back({1, current_index, prev_index});
+        std::iter_swap(current, prev);
+      }
+      else
+      {
+        break;
+      }
+    }
+  }
+}
+
+template <typename iter>
+void get_selection_sort_animation(iter begin, iter end, Animation &animation)
+{
+  bool swapping = true;
+  int begin_index = static_cast<int>(std::distance(begin, begin));
+
+  for (; begin != end && swapping; begin++)
+  {
+    swapping = false;
+    iter smallest = begin;
+    int smallest_index = static_cast<int>(std::distance(begin, smallest));
+
+    for (iter current = smallest; current != end; current++)
+    {
+      int current_index = static_cast<int>(std::distance(begin, current));
+      animation.sorting.push_back({0, current_index, smallest_index});
+      if (*current < *smallest)
+      {
+        animation.sorting.push_back({2, smallest_index, current_index});
+        smallest = current;
+      }
+    }
+
+    if (smallest != end)
+    {
+      swapping = true;
+      animation.sorting.push_back({1, begin_index, smallest_index});
+      std::iter_swap(begin, smallest);
+    }
+  }
 }
 } // namespace nodify
 
